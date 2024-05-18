dbaccf210469970eed5845ea6c07c2c6745827df
David Flores <david.flores@itsoft.mx>
2024-05-18 10:08:13 -0600

    feat: :sparkles: Se agrego la opcion para enviar archivo INO del control de luces

--- controllers/messages.js
+++ controllers/messages.js
@@ -59,23 +59,40 @@ const sendEmailMessage = async (req, res = response) => {
 
         switch (routeName) {
             case 'temperature-dht':
-                projectName = 'Monitor de Temperatura';
+                projectName = 'Monitor de Temperatura - Bluetooth';
                 fileName = 'esp32_bluetooth_to_serial_DHT11.ino';
-                fullSubject = `${subject} - Sensor DHT11 Arduino y ESP32`;
-                sensors.push({
-                    quantity: '1',
-                    name: 'Sensor DHT11 o DHT22'
-                });
+                fullSubject = `${subject} - Sensor DHT11 ESP32`;
+                sensors.push(
+                    { quantity: '1', name: 'Microcontrolador ESP32' },
+                    { quantity: '1', name: 'Sensor DHT11 o DHT22' },
+                    { quantity: '1', name: 'Protoboard para conectar componentes' },
+                    { quantity: '3', name: 'Cables Jumper' },
+                );
                 break;
-        
+            case 'light-control':
+                projectName = 'Control Bidireccional de Led - Bluetooth';
+                fileName = 'esp32_bluetooth_to_serial_led_control.ino';
+                fullSubject = `${subject} - Control de Led ESP32`;
+                sensors.push(
+                    { quantity: '1', name: 'Microcontrolador ESP32' },
+                    { quantity: '1', name: 'LED Diodo emisor de luz (puede ser de cualquier color)' },
+                    { quantity: '1', name: 'Resistencia de 220 ohmios' },
+                    { quantity: '1', name: 'Push Botón de empuje momentáneo' },
+                    { quantity: '1', name: 'Resistencia 10K ohmios para el botón' },
+                    { quantity: '1', name: 'Protoboard para conectar componentes' },
+                    { quantity: '6', name: 'Cables Jumper' },
+
+                );
+                break;
+
             default:
                 break;
         }
-        
+
         console.log({ recipient, fullSubject, projectName, fileName, sensors });
         const info = sendNotificationEmail(recipient, fullSubject, projectName, fileName, sensors)
 
-        if(!info) return res.status(400).json({ ok: false, error})
+        if (!info) return res.status(400).json({ ok: false, error })
 
         res.json({
             ok: true,
@@ -106,22 +123,22 @@ const sendSMSMessage = async (req, res = response) => {
             from: process.env.TWILIO_PHONE_NUMBER,
             to: to
         })
-        .then(response => {
-            console.log(response)
-            
-            res.json({
-                ok: true,
-                msg: 'Mensaje enviado!',
-                response
+            .then(response => {
+                console.log(response)
+
+                res.json({
+                    ok: true,
+                    msg: 'Mensaje enviado!',
+                    response
+                });
+            })
+            .catch(error => {
+                console.log(error)
+                return res.status(400).json({
+                    ok: false,
+                    error
+                })
             });
-        })
-        .catch(error => {
-            console.log(error)
-            return res.status(400).json({
-                ok: false,
-                error
-            })    
-        });
 
 
     } catch (error) {
@@ -142,7 +159,7 @@ const sendVerificationSMSMessage = async (req, res = response) => {
 
         const user = await userModel.findOne({ _id: uid });
 
-        if(!user || !user.phone) {
+        if (!user || !user.phone) {
             return res.status(404).json({
                 ok: false,
                 error: "No se encontrÓ número de teléfono asociado. Favor de Validar"
@@ -150,40 +167,40 @@ const sendVerificationSMSMessage = async (req, res = response) => {
         }
 
         const code = generateRandomNumber();
-        console.log({user});
-        
+        console.log({ user });
+
         const client = new twilio(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
         await client.messages.create({
             body: `Tu código de verificación es: ${code}`,
             from: process.env.TWILIO_PHONE_NUMBER,
             to: `+${user.phone}`
         })
-        .then(async response => {
-            console.log(response);
-            const newCode = new activationCodeModel({
-                userUid: user._id,
-                code: code,
-            });
-            await newCode.save();
+            .then(async response => {
+                console.log(response);
+                const newCode = new activationCodeModel({
+                    userUid: user._id,
+                    code: code,
+                });
+                await newCode.save();
 
-            
-            res.json({
-                ok: true,
-                msg: `Mensaje enviado el número ${user.phone}!`,
-                code,
-                response
+
+                res.json({
+                    ok: true,
+                    msg: `Mensaje enviado el número ${user.phone}!`,
+                    code,
+                    response
+                });
+            })
+            .catch(error => {
+                console.log(error)
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'No fue posible enviar el mensaje SMS',
+                    error
+                })
             });
-        })
-        .catch(error => {
-            console.log(error)
-            return res.status(400).json({
-                ok: false,
-                msg: 'No fue posible enviar el mensaje SMS',
-                error
-            })    
-        });
-        
-        
+
+
 
 
     } catch (error) {
